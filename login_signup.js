f1d5e3a8e25075d6de3c6b1f04655e2ea31665ca
Bange boss <bangegeorgeogeto@gmail.com>
2022-08-15 00:29:33 +0300

    button relative links

--- login_signup.js
+++ login_signup.js
@@ -1,6 +1,6 @@
 function  login() {
-    location.href = "/dashboard.html";
+    location.href = "dashboard.html";
 }
 function signup() {
-    location.href = "/dashboard.html";
+    location.href = "dashboard.html";
 }
