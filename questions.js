06f2ce76a81f3d1156461012d22a3502aa7a007a
Bange boss <bangegeorgeogeto@gmail.com>
2022-08-15 21:08:31 +0300

    added questions part

---
+++ questions.js
@@ -0,0 +1,17 @@
+let index = 0;
+const allQuestions = [
+    "Explain compiled languages",
+    "Explain interpreted languages",
+    "What does href tag do?",
+    "Describe how the internet works in 400 words"
+];
+function showNextQuestion() {
+    if (index < allQuestions.length) {
+        document.getElementById("current_question").innerHTML=allQuestions[index];
+        index++;
+    } else {
+        alert("You have reached end of queue");
+        location.href = "./dashboard.html";
+    }
+
+}
